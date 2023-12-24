55a3fdc15b5db8b2cc1f7c142268977bb2adb6bc
Diego Pereyra <diesphink@gmail.com>
2023-12-23 23:08:08 -0300

    Route pro history

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,14 @@ const router = createRouter({
       component: ConsumeView
     },
     {
+      path: '/history',
+      name: 'history',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/HistoryView.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
